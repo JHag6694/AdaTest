0dff09c747b852c2693f7348aa5d99454f150054
JHag6694 <j.haguet@gmail.com>
2023-06-06 13:15:14 +0200

    Formating problems update

--- messages.adb
+++ messages.adb
@@ -1,17 +1,26 @@
 -- TOP OF FILE
-with Ada.Text_Io;
+with Ada.Text_IO;
 package body Messages is
 
-   use type Ada.Text_Io.Count;
+   use type Ada.Text_IO.Count;
+
+   First_Hello : Boolean := False;
 
    procedure You_Say_Hello is
    begin
-      Ada.Text_Io.Put_Line ("Hello");
+      -- P3 : This comment makes the case instruction is right shifted on format document / selection
+         case First_Hello is
+         when True =>Ada.Text_IO.Put_Line ("Hello");
+         when False =>Ada.Text_IO.Put_Line ("Hello again");
+      end case;
    end;
 
    procedure I_Say_Goodbye is
    begin
-      Ada.Text_Io.Put_Line ("Goodbye");
+      case First_Hello is
+         when True =>null;
+         when False =>Ada.Text_IO.Put_Line ("Goodbye");
+      end case;
    end;
 
 end Messages;
--- messages.ads
+++ messages.ads
@@ -1,6 +1,12 @@
 -- TOP OF FILE
 package Messages is
 
+   procedure Hello_Hello;
+   pragma Obsolescent;
+   -- P1 : Adding a pragma to 1 procedure triggers the package to be displayed as striked
+
+   -- P2 : 'Format selection' on this comment line deleted 2 trailing chars!?
+
    procedure You_Say_Hello;
 
    procedure I_Say_Goodbye;
