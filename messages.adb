1fade35c66851e2ce1e293e76417d1b6bb1a510c
JHag6694 <j.haguet@gmail.com>
2023-12-14 14:43:44 +0100

    Minor simplifications

--- messages.adb
+++ messages.adb
@@ -1,19 +1,23 @@
 -- TOP OF FILE
-with Ada.Text_IO;
+with Ada.Text_Io;
 package body Messages is
 
-   use type Ada.Text_IO.Count;
+   use type Ada.Text_Io.Count;
 
    First_Hello : Boolean := False;
 
    procedure Hello_Hello is null;
 
+   -- #2 : 'Format selection' on this comment line deleted 2 trailing chars
+
    procedure You_Say_Hello is
    begin
-      -- P3 : This comment makes the case instruction is right shifted on format document / selection
+      -- #3 : This comment makes the case instruction is right shifted on 'format document'
       case First_Hello is
-         when True =>Ada.Text_IO.Put_Line ("Hello");
-         when False =>Ada.Text_IO.Put_Line ("Hello again");
+         when True =>
+            Ada.Text_Io.Put_Line ("Hello");
+         when False =>
+            Ada.Text_Io.Put_Line ("Hello again");
       end case;
    end;
 
@@ -21,7 +25,7 @@ package body Messages is
    begin
       case First_Hello is
          when True =>null;
-         when False =>Ada.Text_IO.Put_Line ("Goodbye");
+         when False =>Ada.Text_Io.Put_Line ("Goodbye");
       end case;
    end;
 
--- messages.ads
+++ messages.ads
@@ -2,10 +2,6 @@
 package Messages is
 
    procedure Hello_Hello;
-   pragma Obsolescent;
-   -- P1 : Adding a pragma to 1 procedure triggers the package to be displayed as striked
-
-   -- P2 : 'Format selection' on this comment line deleted 2 trailing chars!?
 
    procedure You_Say_Hello;
 
