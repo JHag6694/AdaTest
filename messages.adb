d0809bbbf9ec9c012e4d245fe32057487bc73c54
JHag6694 <j.haguet@cadwin.com>
2025-12-10 15:45:32 +0100

    Minor adaptation and changes for testing latest extension

--- messages.adb
+++ messages.adb
@@ -2,35 +2,34 @@
 with Ada.Text_Io;
 
 package body Messages is
-  use type Ada.Text_Io.Count;
+   use type Ada.Text_Io.Count;
 
-  First_Hello : Boolean := False;
+   First_Hello : Boolean := False;
 
-  -- #2 : 'Format selection' on this comment line deleted 2 trailing chars
+   -- #2 : 'Format selection' on this comment line deleted 2 trailing chars
 
-  procedure Hello_Hello is null;
+   procedure Hello_Hello is null;
 
-  procedure You_Say_Hello is
-  begin
-    -- #3 : This comment makes the case instruction is right shifted on 'format document'
-    case First_Hello is
-      when True  =>
-        Ada.Text_Io.Put_Line ("Hello");
+   procedure You_Say_Hello is
+   begin
+      -- #3 : This comment makes the case instruction is right shifted on 'format document'
+      case First_Hello is
+         when True =>
+            Ada.Text_Io.Put_Line ("Hello");
 
-      when False =>
-        Ada.Text_Io.Put_Line ("Hello again");
-    end case;
-  end;
+         when False =>
+            Ada.Text_Io.Put_Line ("Hello again");
+      end case;
+   end;
 
-  procedure I_Say_Goodbye is
-  begin
-    case First_Hello is
-      when True  =>
-        null;
-
-      when False =>
-        Ada.Text_Io.Put_Line ("Goodbye");
-    end case;
-  end;
+   procedure I_Say_Goodbye is
+   begin
+      case First_Hello is
+         when True =>
+            null;
+         when False =>
+            Ada.Text_Io.Put_Line ("Goodbye");
+      end case;
+   end;
 
 end Messages;
